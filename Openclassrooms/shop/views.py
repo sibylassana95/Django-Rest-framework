7abc0470d735fb41d2d503b385bccfa119e29c2c
sibylassana95 <sibyamara95@gmail.com>
2023-03-10 13:37:21 +0000

    add Openclassrom

--- Openclassrooms/shop/views.py
+++ Openclassrooms/shop/views.py
@@ -5,16 +5,22 @@ from shop.serializers import CategorySerializer, ProductSerializer
 
 
 class CategoryViewset(ReadOnlyModelViewSet):
-
+ 
     serializer_class = CategorySerializer
-
+ 
     def get_queryset(self):
-        return Category.objects.all()
+        return Category.objects.filter(active=True)
 
 
 class ProductViewset(ReadOnlyModelViewSet):
-
+ 
     serializer_class = ProductSerializer
-
+ 
     def get_queryset(self):
-        return Product.objects.all()
+    # Nous récupérons tous les produits dans une variable nommée queryset
+        queryset = Product.objects.filter(active=True)
+        # Vérifions la présence du paramètre ‘category_id’ dans l’url et si oui alors appliquons notre filtre
+        category_id = self.request.GET.get('category_id')
+        if category_id is not None:
+            queryset = queryset.filter(category_id=category_id)
+        return queryset
