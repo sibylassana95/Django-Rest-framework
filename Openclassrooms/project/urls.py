7e8ab900edbb4b08b8600b507a6ee51d8af598f0
sibylassana95 <sibyamara95@gmail.com>
2023-03-10 13:32:19 +0000

    add Openclassrom

--- Openclassrooms/project/urls.py
+++ Openclassrooms/project/urls.py
@@ -2,13 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
  
-from shop.views import CategoryViewset
+from shop.views import CategoryViewset,ProductViewset
  
 # Ici nous créons notre routeur
 router = routers.SimpleRouter()
 # Puis lui déclarons une url basée sur le mot clé ‘category’ et notre view
 # afin que l’url générée soit celle que nous souhaitons ‘/api/category/’
 router.register('category', CategoryViewset, basename='category')
+router.register('product', ProductViewset, basename='product')
  
 urlpatterns = [
     path('admin/', admin.site.urls),
--- Openclassrooms/shop/views.py
+++ Openclassrooms/shop/views.py
@@ -1,20 +1,20 @@
-from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework.viewsets import ReadOnlyModelViewSet
- 
-from shop.models import Category,Product
-from shop.serializers import CategorySerializer,ProductSerializer
- 
+
+from shop.models import Category, Product
+from shop.serializers import CategorySerializer, ProductSerializer
+
+
 class CategoryViewset(ReadOnlyModelViewSet):
- 
+
     serializer_class = CategorySerializer
- 
+
     def get_queryset(self):
         return Category.objects.all()
 
-class ProductView(APIView):
 
-    def get(self, *args, **kwargs):
-        queryset = Product.objects.all()
-        serializer = ProductSerializer(queryset, many=True)
-        return Response(serializer.data)        
+class ProductViewset(ReadOnlyModelViewSet):
+
+    serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        return Product.objects.all()
